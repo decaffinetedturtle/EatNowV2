00fc3c87003659688c0915343a41fee3b8a023bd
MrOwl1p <122019104+MrOwl1p@users.noreply.github.com>
2023-01-10 17:34:25 +0800

    busuk sial syakir

--- app/src/main/java/com/mad/eatnowv2/userDashboard.java
+++ app/src/main/java/com/mad/eatnowv2/userDashboard.java
@@ -2,13 +2,31 @@ package com.mad.eatnowv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class userDashboard extends AppCompatActivity {
-
+        private Button btn_store;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+        button = (Button) findViewById(R.id.btn_store);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStore(); // this is the method that will open the store activity
+
+            }
+        });
+    }
+    public void openStore(){
+        Intent intent = new Intent(this, Store.class);
+        startActivity(intent);
+    }
+
+    private void startActivity(Intent intent) {
     }
 }
